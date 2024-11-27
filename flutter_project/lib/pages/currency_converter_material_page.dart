a91f28544727be462eed7e2f749e9e4d84d8d7ee
GuthulaSri <srihasini.guthula@innoitlabs.com>
2024-11-27 10:34:23 +0530

    Sized Box

--- flutter_project/lib/pages/currency_converter_material_page.dart
+++ flutter_project/lib/pages/currency_converter_material_page.dart
@@ -36,20 +36,20 @@ class _CurrencyConverterState extends State<CurrencyConverter> {
         ),
 
         body:  Center(
-            child:  Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                  Text(
-                   'INR $result',
-                   style: const TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                                 ),
-                                 ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextField(
+            child:  Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                    Text(
+                     'INR ${result!=0 ? result.toStringAsFixed(2) : result.toStringAsFixed(0)}',
+                     style: const TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                                   ),
+                                   ),
+                  TextField(
                     controller: textEditingController,
                     style: const TextStyle(
                       color: Colors.black,
@@ -70,11 +70,8 @@ class _CurrencyConverterState extends State<CurrencyConverter> {
                          decimal: true,
                        ),
                     ),
-                    // textInputAction: TextInputAction.done,
-                    ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ElevatedButton(onPressed: (){
+                  const SizedBox(height: 10,),
+                  ElevatedButton(onPressed: (){
                     // debug mode, release mode, profile mode
 
                     setState(() {
@@ -90,8 +87,8 @@ class _CurrencyConverterState extends State<CurrencyConverter> {
                       shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))),
                     ),
                     child: const Text('Convert'),),
-                ),
-              ],
+                ],
+              ),
             ),
         ),
       );
